19204f757b28a8ea7ba5192c2c98da752387470b
Yunus Emre Mert <yunusemremert@yandex.com>
2020-12-09 08:16:18 +0300

    MaterialUI Add

--- assets/react/components/TodoTable.js
+++ assets/react/components/TodoTable.js
@@ -1,15 +1,52 @@
 import React, { useContext } from 'react'
+
 import { TodoContext } from '../contexts/TodoContext'
+
+import { IconButton, Table, TableBody, TableCell, TableHead, TableRow, TextField } from '@material-ui/core'
+
+import AddIcon from '@material-ui/icons/Add'
+import EditIcon from '@material-ui/icons/Edit'
+import DeleteIcon from '@material-ui/icons/Delete'
 
 function TodoTable() {
     const context = useContext(TodoContext)
 
     return (
-        <div>
-            {context.todos.map((todo) => (
-                <div key={todo.id}>{todo.name}</div>
-            ))}
-        </div>
+        <Table>
+            <TableHead>
+                <TableRow>
+                    <TableCell>Task</TableCell>
+                    <TableCell align="right">Actions</TableCell>
+                </TableRow>
+            </TableHead>
+            <TableBody>
+                <TableRow>
+                    <TableCell>
+                        <TextField fullWidth={true} />
+                    </TableCell>
+                    <TableCell align="right">
+                        <IconButton>
+                            <AddIcon />
+                        </IconButton>
+                    </TableCell>
+                </TableRow>
+                {context.todos.map((todo) => (
+                    <TableRow key={todo.id}>
+                        <TableCell component="th" scope="row">
+                            {todo.name}
+                        </TableCell>
+                        <TableCell align="right">
+                            <IconButton>
+                                <EditIcon />
+                            </IconButton>
+                            <IconButton>
+                                <DeleteIcon />
+                            </IconButton>
+                        </TableCell>
+                    </TableRow>
+                ))}
+            </TableBody>
+        </Table>
     )
 }
 
